9fd5dc212fbe7ce3e9c65d920330bf1e9c43e77c
altship <1376552345@qq.com>
2024-10-25 00:46:46 +0800

    Function completed. annotation pending.

--- tcp_file_system/src/main.rs
+++ tcp_file_system/src/main.rs
@@ -1,5 +1,5 @@
 use std::str;
-use std::io::prelude::*;
+use std::io::{prelude::*, stdin};
 use std::fs;
 use std::process::Command;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
@@ -14,6 +14,12 @@ fn main() -> std::io::Result<()> {
         if cli.actions.server {SocketAddr::from((Ipv4Addr::UNSPECIFIED, cli.port))}
         else {SocketAddr::from((IpAddr::V4(cli.address.parse().expect("Entered a wrong IPv4 address.")), cli.port))};
     
+
+    if cli.actions.server {
+        server(&addr).expect("Failed to start server.");
+    } else {
+        client(&addr).expect("Failed to start a client");
+    }
     Ok(())
 }
 
@@ -26,25 +32,27 @@ fn server(addr: &SocketAddr) -> Result<(), std::io::Error> {
     let mut command_buff = [0; 16];
     let mut file_name_buff = [0; 32];
     let mut file_buff = [0; 2048];
-    let locat = String::from("~/remote/");
+    let locat = String::from("/home/altship/remote/");
 
     loop {
         let mut byte_read = stream.read(&mut command_buff).expect("Failed in receiving command!");
         if let 0 = byte_read {
             stream.shutdown(std::net::Shutdown::Both).expect("Shutdown connection failed!");
+            println!("Connection closed.");
             return Ok(());
         }
 
         match str::from_utf8(&command_buff[..byte_read]).unwrap() {
             "ls" => {
                 let response = Command::new("ls")
-                                            .arg("-l")
+                                            .arg("-al")
                                             .arg(&locat)
                                             .output()
                                             .expect("Failed to \"ls\"");
                 stream.write(&response.stdout).expect("Failed to write to send buffer!");
                 stream.flush().expect("Failed to flush send buffer!");
             },
+
             "upload" => {
                 byte_read = stream.read(&mut file_name_buff).expect("Failed in receiving file name!");
                 let content_length = stream.read(&mut file_buff).expect("Failed in receiving file!");
@@ -52,6 +60,7 @@ fn server(addr: &SocketAddr) -> Result<(), std::io::Error> {
                 fs::write(format!("{}{}", locat, str::from_utf8(&file_name_buff[..byte_read]).unwrap()), 
                     &file_buff[..content_length]).expect("Writing file error!");
             },
+
             "download" => {
                 byte_read = stream.read(&mut file_name_buff).expect("Failed in receiving file name!");
                 let file_read = fs::read(format!("{}{}", locat, str::from_utf8(&file_name_buff[..byte_read]).unwrap()));
@@ -62,15 +71,80 @@ fn server(addr: &SocketAddr) -> Result<(), std::io::Error> {
                 };
                 stream.write(&file_read).expect("Failed to write to send buffer!");
                 stream.flush().expect("Failed to flush send buffer!");
-            }
+            },
+
             _ => {
                 stream.write("You have entered a wrong command!".as_bytes()).expect("Failed to write to send buffer!");
                 stream.flush().expect("Failed to flush send buffer!");
-            }
+            },
         }
     }
 }
 
 fn client(addr: &SocketAddr) -> Result<(), std::io::Error> {
-    Ok(())
+    let mut stream = TcpStream::connect(addr).expect("Failed to connect to server!");
+    println!("Established connection with server!");
+
+    let mut input_buff = String::new();
+    let mut buff = [0; 2048];
+    let locat = String::from("/home/altship/recv/");
+
+    loop {
+        println!("Please enter a command:");
+        input_buff.clear();
+        stdin().read_line(&mut input_buff).expect("Reading line error!");
+
+        match input_buff.trim() {
+            "ls" => {
+                stream.write("ls".as_bytes()).expect("Failed to write to send buffer!");
+                stream.flush().expect("Failed to flush send buffer!");
+                let byte_read = stream.read(&mut buff).expect("Failed in receiving \"ls\" result!");
+                print!("{}", str::from_utf8(&buff[..byte_read]).unwrap());
+            },
+
+            "upload" => {
+                stream.write("upload".as_bytes()).expect("Failed to write to send buffer!");
+                stream.flush().expect("Failed to flush send buffer!");
+
+                println!("Please enter the file name:");
+                input_buff.clear();
+                stdin().read_line(&mut input_buff).expect("Failed on reading line!");
+                input_buff = input_buff.trim().to_string();
+                stream.write(&input_buff.as_bytes()).expect("Failed to write to send buffer!");
+                stream.flush().expect("Failed on flushing send buffer!");
+
+                let file_read = 
+                    fs::read(format!("{}{}", locat, &input_buff))
+                        .expect("Failed to read file!");
+
+                stream.write(&file_read).expect("Failed on writing to send buffer!");
+                stream.flush().expect("Failed to flush send buffer!");
+            },
+
+            "download" => {
+                stream.write("download".as_bytes()).expect("Failed to write to send buffer!");
+                stream.flush().expect("Failed to flush send buffer!");
+
+                println!("Please enter the file name:");
+                input_buff.clear();
+                stdin().read_line(&mut input_buff).expect("Failed on reading line!");
+                input_buff = input_buff.trim().to_string();
+                stream.write(&input_buff.as_bytes()).expect("Failed to write to send buffer!");
+                stream.flush().expect("Failed on flushing send buffer!");
+
+                let byte_read = stream.read(&mut buff).expect("Failed on receiving file!");
+                fs::write(format!("{}{}", locat, &input_buff), &buff[..byte_read])
+                   .expect("Failed on writing file!");
+            },
+
+            "shutdown" => {
+                stream.shutdown(std::net::Shutdown::Write).expect("Failed to shut connection!");
+                return Ok(());
+            },
+
+            _ => {
+                println!("You have entered a unsupported command!");
+            },
+        }
+    }
 }
