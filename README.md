264aa2365d3d030cebb1e4d73cf9393ec894db95
altship <1376552345@qq.com>
2024-10-24 11:53:57 +0800

    Further synchronization method is still being considered.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Computer Network
 
-Here are the code of my computer network works. It may be with `C` or `rust` version of code.
+Here are the code of my computer network works. They will mainly given with `rust` version.
 
 ## UDP
-A very basic message system based on udp protocol.
+A very basic message system based on udp protocol. Only this program will have `C` language edition.
+
+## Tcp basic file server
+A basic server system implemented "ls" and file's uploading and downloading.
--- tcp_file_system/src/main.rs
+++ tcp_file_system/src/main.rs
@@ -4,10 +4,18 @@ use clap::Parser;
 
 mod argument_parser;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let cli = argument_parser::Cli::parse();
 
     let addr = 
         if cli.actions.server {SocketAddr::from((Ipv4Addr::UNSPECIFIED, cli.port))}
         else {SocketAddr::from((IpAddr::V4(cli.address.parse().expect("Entered a wrong IPv4 address.")), cli.port))};
+    
+    Ok(())
+}
+
+fn server(addr: &SocketAddr) -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind(addr);
+    
+    Ok(())
 }
